692ca889d4e7352af848a495f844eb18bb3a9853
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-04-22 15:04:43 +0100

    file edits

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,13 @@
+//defines a function which takes two parameters: "req" and "res", representing the request and response objects 
 displayName = (req, res) => {
+//create a constant variable called and assign the value of a string 
     const data =
       'What is your name?';
+      //"res" object's "status" method is called with the argument "200" to set the HTTP status code of the response to 200, which indicates a successful response. 
+      //The "send" method is called on the "res" object with the "data" variable as an argument to send the string as the response body.
     res.status(200).send(data);
   };
-  
+  //export the function name so that it can be accessed by the other parts of the application using "require"
   module.exports = {
     displayName,
   };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
-const routes = require('express').Router();
+// This code defines a simple route in a Node.js application using the Express framework.
 
+//Import the 'Router' module from the 'express' library using the 'require' statement and assign it to a constant variable.
+const routes = require('express').Router();
+//Define a 'GET' route for the root ('/') path using the 'routes.get()' method. 
+//Whenever a client makes a 'GET' request to the root path, the callback function with '(req, res)' parameters will be triggered.
 routes.get('/', (req, res) => {
+//In the callback function, the 'res.send()' method is called with the string ' ' as the response body. 
+//The string is sent as the response to the client when the route is accessed.
   res.send('Laurels Echichinwo Jr.');
 });
 
+//The 'routes' object, which represents the defined route, is exported using 'module.exports' so that it can be used in other parts of the application where this module is required.
 module.exports = routes;
