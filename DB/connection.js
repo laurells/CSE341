e05296b84d45798ee7076ebfbe4beccb927c259a
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-05-01 16:34:42 +0100

    ready for deployment

--- DB/connection.js
+++ DB/connection.js
@@ -1,32 +1,46 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+// const MongoClient = require('mongodb').MongoClient;
 const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1g9t77n.mongodb.net/retryWrites=true&w=majority`;
-let _db;
 
-const initDb = (callback) => {
-  if (_db) {
-    console.log('Db is already initialized!');
-    return callback(null, _db);
-  }
-  MongoClient.connect(connectionString)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
+const connectDB = async () => {
+  await mongoose.connect(connectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log('DB connected...!');
 };
 
-const getDb = () => {
-  if (!_db) {
-    throw Error('Db not initialized');
-  }
-  return _db;
-};
+module.exports = connectDB;
 
-module.exports = {
-  initDb,
-  getDb,
-};
+
+
+// let _db;
+
+// const initDb = (callback) => {
+//   if (_db) {
+//     console.log('Db is already initialized!');
+//     return callback(null, _db);
+//   }
+//   MongoClient.connect(connectionString)
+//     .then((client) => {
+//       _db = client;
+//       callback(null, _db);
+//     })
+//     .catch((err) => {
+//       callback(err);
+//     });
+// };
+
+// const getDb = () => {
+//   if (!_db) {
+//     throw Error('Db not initialized');
+//   }
+//   return _db;
+// };
+
+// module.exports = {
+//   initDb,
+//   getDb,
+// };
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongodb = require('./DB/connection');
 //Create an Express application by invoking the 'express()' function and assign it to a constant variable.
 const app = express();
 //Set the port number for the server to listen on. Use the value of the 'PORT' environment variable if available, otherwise use port 3000 as a default.
+mongodb();
 const port = process.env.PORT || 3000;
 // Registers a middleware function using 'app.use()' that handles all incoming HTTP requests with the '/' (root) path and delegates the handling of these requests to a separate file or module located at './routes'.
 app
@@ -15,12 +16,7 @@ app
     })
     .use('/', require('./routes'));
 //use the 'app.listen()' function to listen for incoming requests on the specified port when the server is started. When a request is received, it will trigger the callback function that logs a message to the console indicating which port the server is running on.
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(port, () => {
-            console.log('Connected to DB and listening on port ${port}')
-        })
-    }
+
+app.listen(port, () => {
+    console.log('Server started')
 });
